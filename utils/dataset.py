53bbef21f5cde6a7851513528d8529e5d1346000
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-12-24 15:39:56 +0800

    Integrated dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,6 +1,50 @@
+import torch
+import numpy as np
+import pickle
+import os
+from pathlib import Path
+from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+class TTFSDataset(torch.utils.data.Dataset):
+    def __init__(self, TTFS_path=os.path.join(str(Path.cwd().parent), "asset/ttfs.pkl")):
+         super(TTFSDataset).__init__()
+         with open(TTFS_path,"rb") as f:
+            self.TTFS_data = pickle.load(f)
+    
+    def __getitem__(self, i):
+         return self.TTFS_data[i]
+
+    def __len__(self):
+        return len(self.TTFS_data)
+
+class TTFS():
+    def __init__(self, step_num, device,  root='../data'):
+        self.step_num = step_num
+        self.root = root
+        self.device = device
+
+    def build_TTFS(self, save_path=os.path.join(str(Path.cwd().parent), "asset/ttfs.pkl")):
+        transform = get_transforms("mnist")
+        mnist_train = datasets.MNIST(root=self.root, train=True, download=True, transform=transform)
+        E = np.eye(self.step_num)
+        TTFS_data = []
+        for data, label in mnist_train:
+            data = np.array(data)
+            flatten_img = data.reshape([-1])
+            flatten_img = (flatten_img * 24.5).astype(np.int)
+            ttfs = E[flatten_img]
+            TTFS_data.append(torch.tensor(ttfs.T))
+        with open(save_path, "wb") as f:
+            pickle.dump(TTFS_data, f)
+        print("successfully saved TTFS data")
+
+    def TTFS_to_image(self, ttfs, batches_done):
+        intensity = torch.arange(0, self.step_num).to(device=self.device, dtype=torch.float) / 24
+        intensity = (intensity @ ttfs) / torch.sum(ttfs, dim=0)
+        img = intensity.reshape((1, 28, 28))
+        save_image(img, "images/%d.png" % batches_done,)
 
 def get_transforms(data_name):
     if data_name == "mnist":
@@ -8,7 +52,7 @@ def get_transforms(data_name):
             transforms.Resize((28, 28)),
             transforms.Grayscale(),
             transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))
+            transforms.Normalize((0,), (1,))
             ])
     elif data_name == "cifar10":
         transform = transforms.Compose([
@@ -37,9 +81,5 @@ def get_dataset(batch_size, data_name):
 
 
 if __name__ == "__main__":
-    train_loader, test_loader = get_dataset(128, "cifar10")
-
-    for i, (images, labels) in enumerate(train_loader):
-        print(images.shape)
-        print(labels)
-        break
+    ttfs = TTFS(25, "cpu")
+    ttfs.build_TTFS()
