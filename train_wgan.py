3934bbb15a99c59cd9accfc2cd0a1992421bf3e0
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-12-24 17:44:03 +0800

    Update the training procedure of GANs

--- train_wgan.py
+++ train_wgan.py
@@ -1,56 +1,58 @@
 import torch
-import torch.nn as nn
 from torchvision.utils import save_image
 import numpy as np
 
-from models.snn_gan import Generator, Discriminator
+from models.snn_wgan import Generator, Discriminator
 from utils.dataset import get_dataset
 
 def main():
     n_epochs = 200
     batch_size = 64
     sample_interval = 400
+    clip_value = 0.01
+    n_critic = 1
     device = torch.device('cuda:1')
     dtype = torch.float
+    train_loader,_ = get_dataset(batch_size, "mnist")
+
     noise_dim = 100
     num_steps = 25
     start_grad_step = 24
 
-    train_loader,_ = get_dataset(batch_size, "mnist")
-
     generator = Generator(noise_dim=noise_dim,num_steps=num_steps).to(device=device, dtype=dtype)
     discriminator = Discriminator(num_steps=num_steps).to(device=device, dtype=dtype)
-    optimizer_G = torch.optim.Adam(generator.parameters(), lr=1e-4)
-    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=1e-4)
-    loss = nn.BCELoss()
+    optimizer_G = torch.optim.RMSprop(generator.parameters(), lr=5e-5)
+    optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=5e-5)
 
     for epoch in range(n_epochs):
         for i, (imgs, _) in enumerate(train_loader):
-            valid = torch.ones((num_steps, imgs.shape[0], 1)).to(device=device, dtype=dtype)
-            fake = torch.zeros((num_steps, imgs.shape[0], 1)).to(device=device, dtype=dtype)
             real_imgs = imgs.to(device=device, dtype=dtype)
-
             z = torch.tensor(np.random.normal(0, 1, (imgs.shape[0], noise_dim))).to(device=device, dtype=dtype)
 
-            optimizer_G.zero_grad()
-            gen_imgs = generator(z)
-            g_loss = 0
-            for step in range(start_grad_step, num_steps):
-                g_loss += loss(discriminator(gen_imgs[step]), valid)
-            g_loss = g_loss / (num_steps - start_grad_step)
-            g_loss.backward()
-            optimizer_G.step()
-
             optimizer_D.zero_grad()
+            gen_imgs = generator(z)
             gen_imgs = gen_imgs.detach()
             d_loss = 0
             for step in range(start_grad_step, num_steps):
-                real_loss = loss(discriminator(real_imgs.reshape(batch_size, -1)), valid)
-                fake_loss = loss(discriminator(gen_imgs[step]), fake)
-                d_loss += (fake_loss + real_loss) / 2
+                real_loss = torch.mean(-discriminator(real_imgs.reshape(batch_size, -1)))
+                fake_loss = torch.mean(discriminator(gen_imgs[step]))
+                d_loss += (fake_loss + real_loss)
             d_loss = d_loss / (num_steps - start_grad_step)
             d_loss.backward()
             optimizer_D.step()
+
+            for p in discriminator.parameters():
+                p.data.clamp_(-clip_value, clip_value)
+
+            if i % n_critic == 0:
+                optimizer_G.zero_grad()
+                gen_imgs = generator(z)
+                g_loss = 0
+                for step in range(start_grad_step, num_steps):
+                    g_loss -= torch.mean(discriminator(gen_imgs[step]))
+                g_loss = g_loss / (num_steps - start_grad_step)
+                g_loss.backward()
+                optimizer_G.step()
 
             print(
                 "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
