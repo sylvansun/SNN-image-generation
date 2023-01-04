8d86b2e64580a0fca34f62358ab9b7f8fd01d1f0
maoliyuan <3215024103@qq.com>
2023-01-04 07:26:47 +0000

    add conv snn models and training procedure

---
+++ models/snn_conv_gan.py
@@ -0,0 +1,99 @@
+import snntorch as snn
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(
+        self,
+        noise_dim=100,
+        gen_num_hidden1=128,
+        gen_num_hidden2=256,
+        gen_num_hidden3=512,
+        gen_num_hidden4=1024,
+        gen_num_outputs=28 * 28,
+        num_steps=25,
+        beta=0.95,
+    ):
+        super().__init__()
+        self.num_steps = num_steps
+        self.fc1 = nn.Linear(noise_dim, gen_num_hidden1)
+        self.lif1 = snn.Leaky(beta=beta)
+        self.fc2 = nn.Linear(gen_num_hidden1, gen_num_hidden2)
+        self.lif2 = snn.Leaky(beta=beta)
+        self.fc3 = nn.Linear(gen_num_hidden2, gen_num_hidden3)
+        self.lif3 = snn.Leaky(beta=beta)
+        self.fc4 = nn.Linear(gen_num_hidden3, gen_num_hidden4)
+        self.lif4 = snn.Leaky(beta=beta)
+        self.fc5 = nn.Linear(gen_num_hidden4, gen_num_outputs)
+        self.lif5 = snn.Leaky(beta=beta)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+        mem3 = self.lif3.init_leaky()
+        mem4 = self.lif4.init_leaky()
+        mem5 = self.lif5.init_leaky()
+
+        spk5_rec = []
+        mem5_rec = []
+
+        for step in range(self.num_steps):
+            cur1 = self.fc1(x)
+            spk1, mem1 = self.lif1(cur1, mem1)
+            cur2 = self.fc2(spk1)
+            spk2, mem2 = self.lif2(cur2, mem2)
+            cur3 = self.fc3(spk2)
+            spk3, mem3 = self.lif3(cur3, mem3)
+            cur4 = self.fc4(spk3)
+            spk4, mem4 = self.lif4(cur4, mem4)
+            cur5 = self.fc5(spk4)
+            spk5, mem5 = self.lif5(cur5, mem5)
+            spk5_rec.append(self.tanh(spk5))
+            mem5_rec.append(self.tanh(mem5))
+            
+
+        return torch.stack(spk5_rec, dim=0), torch.stack(mem5_rec, dim=0)
+
+
+class Discriminator(nn.Module):
+    def __init__(
+        self, dis_inputs=28 * 28, dis_num_hidden1=512, dis_num_hidden2=256, dis_num_outputs=1, num_steps=25, beta=0.95
+    ):
+        super().__init__()
+        self.num_steps = num_steps
+        self.conv1 = nn.Conv2d(1, 12, 5)
+        self.maxpool = nn.MaxPool2d(2)
+        self.lif1 = snn.Leaky(beta=beta)
+        self.conv2 = nn.Conv2d(12, 64, 5)
+        self.lif2 = snn.Leaky(beta=beta)
+        self.fc1 = nn.Linear(64*4*4, dis_num_outputs)
+        self.lif3 = snn.Leaky(beta=beta)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+        mem3 = self.lif3.init_leaky()
+
+        spk3_rec = []
+        mem3_rec = []
+
+        for step in range(self.num_steps):
+            cur1 = self.maxpool(self.conv1(x.view(64,1,28,28)))
+            spk1, mem1 = self.lif1(cur1, mem1)
+            cur2 = self.maxpool(self.conv2(spk1))
+            spk2, mem2 = self.lif2(cur2, mem2)
+            cur3 = self.fc1(spk2.view(64,-1))
+            spk3, mem3 = self.lif3(cur3, mem3)
+            spk3_rec.append(self.sigmoid(spk3))
+            mem3_rec.append(self.sigmoid(mem3))
+
+        return torch.stack(spk3_rec, dim=0), torch.stack(mem3_rec, dim=0)
+
+if __name__ == "__main__":
+    gen = Generator()
+    dis = Discriminator()
+    print(gen)
+    print(dis)
---
+++ train_conv_gan.py
@@ -0,0 +1,90 @@
+import os
+import argparse
+
+import torch
+import torch.nn as nn
+from torchvision.utils import save_image
+import numpy as np
+
+from models.snn_conv_gan import Generator, Discriminator
+from utils.dataset import get_dataset
+from utils.etqdm import etqdm
+
+
+def main(args):
+    n_epochs = args.n_epochs
+    batch_size = args.batch_size
+    sample_interval = args.sample_interval if not args.vis else 1
+    device = torch.device(f"cuda:{args.gpu}")
+    num_steps = args.num_steps
+    start_grad_step = args.start_grad_step
+    output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
+    vis = args.vis
+    dtype = torch.float
+    noise_dim = 100
+    num_vis = 25
+
+    train_loader, _ = get_dataset(batch_size, "mnist")
+
+    generator = Generator(noise_dim=noise_dim, num_steps=num_steps).to(device=device, dtype=dtype)
+    discriminator = Discriminator(num_steps=num_steps).to(device=device, dtype=dtype)
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=3e-4)
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=3e-4)
+    loss = nn.BCELoss()
+
+    for epoch in range(n_epochs):
+        train_bar = etqdm(train_loader, desc=f"Epoch {epoch:03d}")
+        for i, (imgs, _) in enumerate(train_bar):
+            valid = torch.ones((num_steps, imgs.shape[0], 1)).to(device=device, dtype=dtype)
+            fake = torch.zeros((num_steps, imgs.shape[0], 1)).to(device=device, dtype=dtype)
+            real_imgs = imgs.to(device=device, dtype=dtype)
+
+            z = torch.tensor(np.random.normal(0, 1, (imgs.shape[0], noise_dim))).to(device=device, dtype=dtype) #BS*noise_dim
+            optimizer_G.zero_grad()
+            _, gen_imgs = generator(z) # num_steps * BS * 28^2
+            g_loss = 0
+            for step in range(start_grad_step, num_steps):
+                _, pred = discriminator(gen_imgs[step])
+                g_loss += loss(pred, valid)
+            g_loss = g_loss / (num_steps - start_grad_step)
+            g_loss.backward()
+            optimizer_G.step()
+
+            optimizer_D.zero_grad()
+            gen_imgs = gen_imgs.detach()
+            d_loss = 0
+            for step in range(start_grad_step, num_steps):
+                _, real_pred = discriminator(real_imgs.reshape(batch_size, -1))
+                _, fake_pred = discriminator(gen_imgs[step])
+                real_loss = loss(real_pred, valid)
+                fake_loss = loss(fake_pred, fake)
+                d_loss += (fake_loss + real_loss) / 2
+            d_loss = d_loss / (num_steps - start_grad_step)
+            d_loss.backward()
+            optimizer_D.step()
+
+            train_bar.set_postfix_str(f"D_loss: {d_loss.item() / 2:.4f}, G_loss: {g_loss.item():.4f}")
+            batches_done = epoch * len(train_loader) + i
+            if batches_done % sample_interval == 0:
+                image = gen_imgs.data[-1, :num_vis].reshape([num_vis, 1, 28, 28])
+                real_image = real_imgs.data[:25, 0].reshape([-1, 1, 28, 28])
+                image = torch.cat([real_image, image], dim=0)
+                if vis:
+                    save_image(image, os.path.join(output_dir, "convgan.png"), nrow=5, normalize=True)
+                else:
+                    save_image(image, os.path.join(output_dir, "convgan_%d.png" % batches_done), nrow=5, normalize=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--gpu", type=int, default=0)
+    parser.add_argument("-b", "--batch_size", type=int, default=64)
+    parser.add_argument("-e", "--n_epochs", type=int, default=200)
+    parser.add_argument("-n", "--num_steps", type=int, default=25)
+    parser.add_argument("-s", "--start_grad_step", type=int, default=20)
+    parser.add_argument("-i", "--sample_interval", type=int, default=400)
+    parser.add_argument("-o", "--output_dir", type=str, default="images/convgan")
+    parser.add_argument("--vis", action="store_true")
+    args = parser.parse_args()
+    main(args)
