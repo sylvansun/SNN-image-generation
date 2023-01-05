4d73d4f900cac6a3f3a73a9c3fa9810481023a75
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2023-01-05 21:43:41 +0800

    Modify multiple generators

--- dcgan.py
+++ dcgan.py
@@ -44,6 +44,117 @@ def weights_init_normal(m):
         torch.nn.init.constant_(m.bias.data, 0.0)
 
 
+class GenMid(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.init_size = opt.img_size // 4
+        self.Num = 200
+        self.l1 = nn.Linear(opt.latent_dim, 128 * self.init_size ** 2)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.up1 = nn.Upsample(scale_factor=2)
+        self.conv1 = nn.Conv2d(self.Num * 128, 128, 3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128, 0.8)
+        self.lrelu1 = nn.LeakyReLU(0.2, inplace=True)
+        self.up2 = nn.Upsample(scale_factor=2)
+        self.conv2 = nn.Conv2d(128, 64, 3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64, 0.8)
+        self.lrelu2 = nn.LeakyReLU(0.2, inplace=True)
+        self.conv3 = nn.Conv2d(64, opt.channels, 3, stride=1, padding=1)
+        self.fc_last = nn.Linear(1024, 1024)
+        self.conv_last = nn.Conv2d(self.Num, 1, 3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+        self.lif1 = snn.Leaky(beta=0.95)
+
+    def forward(self, z):
+        B = z.shape[0]
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        out = self.bn1(out)
+        out = self.up1(out)
+        mem = self.lif1.init_leaky()
+        spk_rec = []
+        mem_rec = []
+        for _ in range(self.Num):
+            spk, mem = self.lif1(out, mem)
+            spk_rec.append(spk)
+            mem_rec.append(mem)
+        out = torch.stack(spk_rec, dim=0).reshape(B, -1, 16, 16)
+
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.lrelu1(out)
+        out = self.up2(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.lrelu2(out)
+        out = self.conv3(out)
+
+
+
+        # out = self.conv_last(spk_rec.transpose(0, 1).reshape(B, -1, 32, 32))
+        # out = F.relu(out)
+        # out = self.fc_last(out.view(B, -1))
+        # out = out.view(B, 1, 32, 32)
+        out = self.tanh(out)
+        return out
+
+
+class Gen2(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.init_size = opt.img_size // 4
+        self.Num = 100
+        self.l1 = nn.Linear(opt.latent_dim, 128 * self.init_size ** 2)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.up1 = nn.Upsample(scale_factor=2)
+        self.conv1 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128, 0.8)
+        self.lrelu1 = nn.LeakyReLU(0.2, inplace=True)
+        self.up2 = nn.Upsample(scale_factor=2)
+        self.conv2 = nn.Conv2d(self.Num * 128, 64, 3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64, 0.8)
+        self.lrelu2 = nn.LeakyReLU(0.2, inplace=True)
+        self.conv3 = nn.Conv2d(64, opt.channels, 3, stride=1, padding=1)
+        self.fc_last = nn.Linear(1024, 1024)
+        self.conv_last = nn.Conv2d(self.Num, 1, 3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+        self.lif1 = snn.Leaky(beta=0.95)
+
+    def forward(self, z):
+        B = z.shape[0]
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        out = self.bn1(out)
+        out = self.up1(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.lrelu1(out)
+        out = self.up2(out)
+        
+        mem = self.lif1.init_leaky()
+        spk_rec = []
+        mem_rec = []
+        for _ in range(self.Num):
+            spk, mem = self.lif1(out, mem)
+            spk_rec.append(spk)
+            mem_rec.append(mem)
+        out = torch.stack(spk_rec, dim=0).reshape(B, -1, 32, 32)
+        
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.lrelu2(out)
+        out = self.conv3(out)
+
+        # out = self.conv_last(spk_rec.transpose(0, 1).reshape(B, -1, 32, 32))
+        # out = F.relu(out)
+        # out = self.fc_last(out.view(B, -1))
+        # out = out.view(B, 1, 32, 32)
+        out = self.tanh(out)
+        return out
+
+
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
@@ -91,16 +202,16 @@ class Generator(nn.Module):
         spk_rec = torch.stack(spk_rec, dim=0) # [N, B, C, H, W]
         mem_rec = torch.stack(mem_rec, dim=0)
 
-        img = self.conv_last(spk_rec.transpose(0, 1).reshape(B, -1, 32, 32))
-        # img = F.relu(img)
-        # img = self.fc_last(img.view(B, -1))
-        out = img.view(B, 1, 32, 32)
-        img = self.tanh(out)
-        return img
+        # out = self.conv_last(spk_rec.transpose(0, 1).reshape(B, -1, 32, 32))
+        # out = F.relu(out)
+        # out = self.fc_last(out.view(B, -1))
+        # out = out.view(B, 1, 32, 32)
+        out = self.tanh(out)
+        return out
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, args=None):
         super(Discriminator, self).__init__()
 
         self.conv1 = nn.Conv2d(opt.channels, 16, 3, 2, 1)
@@ -164,7 +275,7 @@ class Discriminator(nn.Module):
 adversarial_loss = torch.nn.BCELoss()
 
 # Initialize generator and discriminator
-generator = Generator()
+generator = Gen2()
 discriminator = Discriminator()
 
 if cuda:
@@ -214,7 +325,7 @@ for epoch in range(opt.n_epochs):
         # -----------------
         #  Train Generator
         # -----------------
-        for _ in range(10):
+        for _ in range(1):
             optimizer_G.zero_grad()
 
             # Sample noise as generator input
